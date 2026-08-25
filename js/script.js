3a3bb9bc4aa76f1699ccd8499c4b8215fe6b9eaf
MAKOLI21 <jojimakoli21@gmail.com>
2026-08-25 19:34:39 +0300

    Initial commit: clear root structure

---
+++ js/script.js
@@ -0,0 +1,20 @@
+const burger = document.querySelector('#burger')
+const nav = document.querySelector('.header__nav')
+
+burger.addEventListener('click', () => {
+  nav.classList.toggle('open');
+});
+
+const themeBtn = document.querySelector('.theme-btn')
+const themeIcon = themeBtn.querySelector('img');
+
+themeBtn.addEventListener('click', () => {
+  document.body.classList.toggle('dark-theme')
+  if(document.body.classList.contains('dark-theme')){
+    themeIcon.src = '/js/img/2.svg'
+  }else{
+    themeIcon.src = '/js/img/moon-svgrepo-com.svg';
+  }
+})
+
+
